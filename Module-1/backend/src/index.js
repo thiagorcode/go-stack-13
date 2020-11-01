62c89efb2dde241afa9720c4437b200cb595334a
thiagorcode_n <54317829+thiagorcoden12@users.noreply.github.com>
2020-11-01 01:42:17 -0300

    Node.js- 1 - End

--- Module-1/backend/src/index.js
+++ Module-1/backend/src/index.js
@@ -1,11 +1,40 @@
 const express = require('express');
-const { uuid } = require('uuidv4')
+const { uuid, isUuid } = require('uuidv4')
 
 const app = express();
 
 app.use(express.json());
 
 const projects = []
+
+/**
+ * Middlewares => Interceptador de requisições que interromper totalmente a requisição ou alterar os
+ * dados da requisição.
+ * 
+ */
+
+function logReq(req, res, next) {
+  const { method, url } = req;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+  console.log(logLabel);
+
+  console.time(logLabel);
+  return next(); // Próximo middleware
+
+  console.timeEnd(logLabel); // Executado por úçtimo 
+}
+
+function validateProjectId(req, res, next) {
+  const { id } = req.params;
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: 'Invalid project Id!' });
+  }
+  return next();
+}
+
+app.use(logReq)
+app.use('/projects/:id', validateProjectId)
 
 app.get('/projects', (req, res) => {
   const { title } = req.query;
@@ -52,7 +81,7 @@ app.put('/projects/:id', (req, res) => {
 
 app.delete('/projects/:id', (req, res) => {
   const { id } = req.params;
-  const { title, owner } = req.body;
+
   const projectIndex = projects.findIndex(project => project.id === id);
 
   const validation = projectIndex < 0 ? true : false;
