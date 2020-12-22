8c79a42c3b6915c93989627d902a2e08b832402f
thiagorcode <ti.thiago.rodrigues@outlook.com>
2020-12-22 02:04:09 -0300

    Listando dados da API

---
+++ Module-1/ReactNative/mobile/src/index.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+
+import api from './services/api';
+
+// * Não possuem valor semântico (significado)
+// * Não possuem estilização própria
+// * Todos componentes possuem por padrão "display flex"
+// * View: div, footer, header, main, aside, section
+// * Text: p, span, strong, h1, h2, h3
+// * Não existe herança de estilização
+
+export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      console.log(response.data);
+      setProjects(response.data)
+    })
+  }, [])
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <View style={styles.container} >
+        {projects.map(project => {
+          <Text key={project.id}>{project.title}</Text>
+        })}
+      </ View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    color: "#FFF",
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+});
