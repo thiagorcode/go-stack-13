995efafb28182fbc47a316a1bb8cf73799eb3222
thiagorcode <ti.thiago.rodrigues@outlook.com>
2020-12-28 23:46:42 -0300

    ReactNative - Concluded

--- Module-1/ReactNative/mobile/src/index.js
+++ Module-1/ReactNative/mobile/src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 
 import api from './services/api';
 
@@ -15,19 +15,41 @@ export default function App() {
 
   useEffect(() => {
     api.get('projects').then(response => {
-      console.log(response.data);
-      setProjects(response.data)
+      setProjects(response.data);
     })
-  }, [])
+  }, []);
 
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novoprojeto ${Date.now()}`,
+      owner: 'Diego Fernandes',
+    })
+    const project = response.data;
+    setProjects([...projects, project])
+  }
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <View style={styles.container} >
-        {projects.map(project => {
-          <Text key={project.id}>{project.title}</Text>
-        })}
-      </ View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item: project }) => (<Text style={styles.project}>{project.title}</Text>)}
+        />
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar Projeto</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+
+      {/* <View style={styles.container} >
+        <Text>DADOS DA API:</Text>
+        {projects.map(project => <Text style={styles.project} key={project.id}>{project.title}</Text>
+        )}
+      </View> */}
     </>
   );
 };
@@ -36,12 +58,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center'
+
   },
   title: {
     color: "#FFF",
     fontSize: 32,
     fontWeight: 'bold',
   },
+  project: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  }
 });
