a5f4f84671a55f8401f42aa57efe8e676b1f036e
Thiago <ti.thiago.rodrigues@outlook.com>
2021-01-26 16:26:51 -0300

    Rotas auteticadas

--- Level-2/Iniciando-Back-End/src/services/AuthenticateUserService.ts
+++ Level-2/Iniciando-Back-End/src/services/AuthenticateUserService.ts
@@ -2,6 +2,8 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
+
 import User from '../models/User';
 
 interface Request {
@@ -30,9 +32,11 @@ export default class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, '57c911d1d4dbe15c4c78b40e3dbcc414', {
+    const { expiresIn, secret } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return { user, token };
