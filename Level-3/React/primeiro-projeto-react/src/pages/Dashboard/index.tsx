406ebe7b9d6cf35dd2f5b45aaf35d63747c7dc48
thiagorcode <ti.thiago.rodrigues@outlook.com>
2021-02-10 11:51:00 -0300

    estilizando dashboard

--- Level-3/React/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ Level-3/React/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
-import { Title } from './style';
+import { FiChevronRight } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no Github</Title>;
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './style';
+// 17:23
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Git Hub Explorer" />
+    <Title>Explore repositórios no Github</Title>
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+    <Repositories>
+      <a href="Teste">
+        <img src="https://avatars.githubusercontent.com/u/54317829?s=460&u=4be6d3b6128129b992f3b6b0673d6d4ae3b9f6ae&v=4" alt="Foto de perfil" />
+        <div>
+          <strong>thiagorcode/ElectronStudies</strong>
+          <p>Estudos de ElectronJS</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+
+  </>
+);
 
 export default Dashboard;
