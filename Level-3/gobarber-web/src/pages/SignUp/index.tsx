12ffdef5db0b00c913bf38c6bbf13061fe95c211
thiagorcode <ti.thiago.rodrigues@outlook.com>
2021-02-23 11:22:14 -0300

    usabilidade input

---
+++ Level-3/gobarber-web/src/pages/SignUp/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  FiArrowLeft, FiUser, FiMail, FiLock,
+} from 'react-icons/fi';
+import { Form } from '@unform/web';
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: any): void {
+    console.log(data);
+  }
+  return (
+
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} placeholder="E-mail" />
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="signup">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
--- Level-3/gobarber-web/src/pages/SignUp/styles.ts
+++ Level-3/gobarber-web/src/pages/SignUp/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-import signInBackground from '../../assets/sign-in-background.png';
+import signUpBackground from '../../assets/sign-up.png';
 
 export const Container = styled.div`
   height: 100vh;
@@ -34,9 +34,9 @@ export const Content = styled.div`
         color: ${shade(0.2, '#f4ede8')};
       }
     }
-
+  }
   > a {
-    color: #ff9000;
+    color: #f4ede8;
     display: block;
     margin-top: 24px;
     text-decoration: none;
@@ -47,13 +47,13 @@ export const Content = styled.div`
       margin-right: 16px;
     }
     &:hover {
-      color: ${shade(0.2, '#ff9000')};
+      color: ${shade(0.2, '#f4ede8')};
     }
   }
 `;
 
 export const Background = styled.div`
   flex: 1;
-  background: url(${signInBackground}) no-repeat center;
+  background: url(${signUpBackground}) no-repeat center;
   background-size: cover;
 `;
