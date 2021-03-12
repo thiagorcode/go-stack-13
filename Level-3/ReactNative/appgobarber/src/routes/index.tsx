08f440bc2c35a25eee1039824ddcd161104a5894
Thiago <ti.thiago.rodrigues@outlook.com>
2021-03-12 00:38:33 -0300

    Estilização do SingIn

---
+++ Level-3/ReactNative/appgobarber/src/routes/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+// @createStackNavigator Gerencia nossa navegação por pilha
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+import SignIn from '../pages/SingIn';
+import SignUp from '../pages/SignUp';
+
+const Auth = createStackNavigator();
+
+const AuthRoutes: React.FC = () => {
+  return (
+    <Auth.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: '#312e38' }
+      }}
+    >
+      <Auth.Screen name="SignIn" component={SignIn} />
+      <Auth.Screen name="SignUp" component={SignUp} />
+
+    </Auth.Navigator>
+  )
+}
+
+export default AuthRoutes;
