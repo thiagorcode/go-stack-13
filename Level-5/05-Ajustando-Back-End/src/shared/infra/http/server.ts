2699a547a274ae96f9c684ae0d2d03034eda29f5
Thiago <ti.thiago.rodrigues@outlook.com>
2021-05-11 22:29:34 -0300

    create: Novo módulo adicionado

---
+++ Level-5/05-Ajustando-Back-End/src/shared/infra/http/server.ts
@@ -0,0 +1,51 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import { errors } from 'celebrate';
+import 'express-async-errors';
+
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import rateLimiter from './middleware/rateLimiter';
+import routes from './routes';
+import '@shared/infra/typeorm';
+import '@shared/container';
+
+const PORT_LOCAL = 3333;
+
+const app = express();
+
+app.use(rateLimiter);
+app.use(cors());
+app.use(express.json());
+// localhost:PORT/files/NOME_DA_IMAGEM
+app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(routes);
+app.use(errors());
+
+// Tratativas de erros
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'helloworld' });
+});
+
+app.listen(PORT_LOCAL, () => {
+  console.log(`Server started on port ${PORT_LOCAL}`);
+});
