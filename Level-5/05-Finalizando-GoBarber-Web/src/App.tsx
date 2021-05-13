3c8d2283271da1fbe61b284cc88f7dc947568d94
Thiago <ti.thiago.rodrigues@outlook.com>
2021-05-12 21:08:31 -0300

    Adicionado Pasta Finalizando GoBarber

---
+++ Level-5/05-Finalizando-GoBarber-Web/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Routes from './routes';
+
+import AppProvider from './hooks/index';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+
+  <Router>
+
+    <AppProvider>
+      <Routes />
+    </AppProvider>
+
+    <GlobalStyle />
+  </Router>
+
+);
+
+export default App;
