7e550293efb7ed0f7a444a49093a5b99b94f7ced
Thiago <ti.thiago.rodrigues@outlook.com>
2021-08-13 20:46:03 -0300

    up

--- Level-5/05-Finalizando-GoBarber-Web/src/pages/Profile/index.tsx
+++ Level-5/05-Finalizando-GoBarber-Web/src/pages/Profile/index.tsx
@@ -46,13 +46,13 @@ const Profile: React.FC = () => {
             .email('Digite um e-mail válido'),
           old_password: Yup.string(),
           password: Yup.string().when('old_password', {
-            is: (val) => !!val.length,
+            is: (val: string) => !!val.length,
             then: Yup.string().required('Campo obrigatório'),
             otherwise: Yup.string(),
           }),
           password_confirmation: Yup.string()
             .when('old_password', {
-              is: (val) => !!val.length,
+              is: (val: string) => !!val.length,
               then: Yup.string().required('Campo obrigatório'),
               otherwise: Yup.string(),
             })
