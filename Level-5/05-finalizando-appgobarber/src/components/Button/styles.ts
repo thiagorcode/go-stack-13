b58d7ab0d6f35fd6e4b4b973fb2afcd7630090b6
Thiago Rodrigues <ti.thiago.rodrigues@outlook.com>
2021-09-10 19:03:39 -0300

    appGobarber

---
+++ Level-5/05-finalizando-appgobarber/src/components/Button/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled(RectButton)`
+  height: 60px;
+  background: #ff9000;
+  border-radius: 10px;
+  margin-top: 8px;
+  width: 100%;
+  flex-direction: row;
+  place-content: center;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonText = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  color: #312e38;
+  font-size: 18px;
+`;
